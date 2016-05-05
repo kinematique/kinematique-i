9758890c7444f593ba0399ccdf007bcf6c8585be
Brian Hill <brian@lingerhere.org>
2016-05-04 21:40:59 -0700

    Added labels to the vectors.

--- Kinematique I/PlaneView.swift
+++ Kinematique I/PlaneView.swift
@@ -26,7 +26,17 @@ let nominalHeadLength: CGFloat = 20
 let tailWidth: CGFloat = 4
 let vectorStrokeWidth: CGFloat = 1
 
+// Constants for drawing labels
+let labelProximity: CGFloat = 24
+let labelTextColor = UIColor(white: 0.5, alpha: 1.0).CGColor
+let labelFont = UIFont.systemFontOfSize(24)
+let labelCenteringAdjustment: CGFloat = 12
+
 class PlaneView: UIView {
+    
+    static var fullHeight: CGFloat = 1000 // a placeholder
+    
+    var scale: CGFloat = 1.0
     
     let model = Model.sharedInstance
     
@@ -47,18 +57,22 @@ class PlaneView: UIView {
     }
     
     private func _addAxes(context: CGContext, forOrigin origin: CGPoint, frameSize size: CGSize) {
+        CGContextBeginPath(context)
         CGContextMoveToPoint(context, origin.x, 0)
-        CGContextAddLineToPoint(context, origin.x, size.height)
+        CGContextAddLineToPoint(context, origin.x, size.height / scale)
         CGContextMoveToPoint(context, 0, origin.y)
-        CGContextAddLineToPoint(context, size.width, origin.y)
+        CGContextAddLineToPoint(context, size.width / scale, origin.y)
+        CGContextDrawPath(context, .Stroke)
     }
     
     private func _addCircle(context: CGContext, atPoint point: CGPoint) {
         let circleRect = CGRectMake(point.x - pointRadius, point.y - pointRadius, 2 * pointRadius, 2 * pointRadius)
+        CGContextBeginPath(context)
         CGContextAddEllipseInRect(context, circleRect)
+        CGContextDrawPath(context, .FillStroke)
     }
     
-    private func _drawVector(context: CGContext, length: CGFloat) {
+    private func _addCanonicalVector(context: CGContext, length: CGFloat) {
         let headLength: CGFloat = nominalHeadLength < length / 2 ? nominalHeadLength : length / 2
         let bodyLength: CGFloat = length - headLength
         CGContextBeginPath(context)
@@ -70,8 +84,9 @@ class PlaneView: UIView {
         CGContextAddLineToPoint(context, bodyLength, -tailWidth / 2)
         CGContextAddLineToPoint(context, -tailWidth / 2, -tailWidth / 2)
         CGContextClosePath(context)
+        CGContextDrawPath(context, .FillStroke)
     }
-    
+
     private func _addVector(context: CGContext, fromOrigin origin: CGPoint, toPoint point: CGPoint) {
         let dy = point.y - origin.y
         let dx = point.x - origin.x
@@ -80,45 +95,69 @@ class PlaneView: UIView {
         CGContextSaveGState(context)
         CGContextTranslateCTM(context, origin.x, origin.y)
         CGContextRotateCTM(context, angle)
-        _drawVector(context, length: length)
+        _addCanonicalVector(context, length: length)
+        CGContextRestoreGState(context)
+    }
+    
+    private func _labelVector(context: CGContext, fromOrigin origin: CGPoint, adjustedAngle: CGFloat, halfLength: CGFloat, attributes: [String: AnyObject], label: String) {
+        let attributedString = NSAttributedString(string: label, attributes: attributes)
+        let line = CTLineCreateWithAttributedString(attributedString)
+        CGContextSaveGState(context)
+        CGContextTranslateCTM(context, origin.x - labelCenteringAdjustment, origin.y + labelCenteringAdjustment)
+        let transform = CGAffineTransformMakeScale(1, -1)
+        CGContextConcatCTM(context, transform)
+        CGContextSetTextPosition(context, halfLength * cos(adjustedAngle), -halfLength * sin(adjustedAngle))
+        CTLineDraw(line, context)
         CGContextRestoreGState(context)
     }
 
     override func drawRect(rect: CGRect) {
         let context = UIGraphicsGetCurrentContext()!
+        
+        CGContextSaveGState(context)
+        let transform = CGAffineTransformMakeScale(scale, scale)
+        CGContextConcatCTM(context, transform)
 
         // Add axes if the origin has been set.
         if let origin = model.origin {
             CGContextSetFillColorWithColor(context, axesStrokeColor)
             CGContextSetLineWidth(context, axesWidth)
-            CGContextBeginPath(context)
             _addAxes(context, forOrigin: origin, frameSize: frame.size)
-            CGContextDrawPath(context, .Stroke)
         }
 
         // Add the points as filled gray circles with a thin stroke
-        CGContextBeginPath(context)
-        CGContextSetFillColorWithColor(context, pointsFillColor)
-        CGContextSetStrokeColorWithColor(context, pointsStrokeColor)
-        CGContextSetLineWidth(context, pointsStrokeWidth)
         for point in model.points {
-            CGContextBeginPath(context)
+            CGContextSetFillColorWithColor(context, pointsFillColor)
+            CGContextSetStrokeColorWithColor(context, pointsStrokeColor)
+            CGContextSetLineWidth(context, pointsStrokeWidth)
             _addCircle(context, atPoint: point)
-            CGContextDrawPath(context, .FillStroke)
-
         }
-        CGContextDrawPath(context, .FillStroke)
         
-        // Add the vectors as red arrows
-        CGContextBeginPath(context)
+        // Add the vectors
         CGContextSetFillColorWithColor(context, vectorFillColor)
         CGContextSetStrokeColorWithColor(context, vectorStrokeColor)
         CGContextSetLineWidth(context, vectorStrokeWidth)
         for point in model.points {
-            CGContextBeginPath(context)
             _addVector(context, fromOrigin: model.origin!, toPoint: point)
-            CGContextDrawPath(context, .FillStroke)
         }
+        
+        // Label the vectors
+        let attributes: [String: AnyObject] = [
+            NSForegroundColorAttributeName : labelTextColor,
+            NSFontAttributeName: labelFont
+        ]
+        var labelCount: Int = 0
+        for point in model.points {
+            let dy = point.y - model.origin!.y
+            let dx = point.x - model.origin!.x
+            let halfLength: CGFloat = sqrt(dx * dx + dy * dy) / 2
+            let adjustedAngle: CGFloat = atan2(dy, dx) + labelProximity / halfLength
+            labelCount += 1
+            let label = String(labelCount)
+            _labelVector(context, fromOrigin: model.origin!, adjustedAngle: adjustedAngle, halfLength: halfLength, attributes: attributes, label: label)
+        }
+        
+        CGContextRestoreGState(context)
         
     }
 
